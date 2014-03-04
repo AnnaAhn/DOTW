f2e5f6de6398118255efea13c1f8a1789c247ad3
Naveen <knaveen87@gmail.com>
2014-03-04 08:14:43 -0800

    Separate main method

---
+++ MovingWindowMain.java
@@ -0,0 +1,46 @@
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.util.Random;
+
+public class MovingWindowMain {
+
+	public static void main(String args[])throws Exception
+	{	
+		//Find the width and height
+		Dimension d=Toolkit.getDefaultToolkit().getScreenSize();
+		Point p=new Point(d.width/2,d.height/2);
+		int numWin=2;
+		int range=10; // Range of the velocities
+		if(args.length>0)
+			numWin=Integer.parseInt(args[0]);
+
+		MultipleMovingWindow window[] =new MultipleMovingWindow[numWin];
+
+		for(int win=0;win<numWin;win++)
+		{
+			//Assign a random velocity to this tiny window
+			Random r=new Random();
+			double velx=r.nextGaussian()*range;
+			double vely=r.nextGaussian()*range;
+
+			System.out.println(velx+" "+vely);
+
+			window[win]=new MultipleMovingWindow(velx,vely,d);
+			window[win].setLocation(p);
+			window[win].setVisible(true);	
+		}
+
+		try
+		{
+			Thread.sleep(1000);
+		}
+		catch(Exception e)
+		{}
+
+		for(int win=0;win<numWin;win++)
+			window[win].runAnimation();
+		
+
+	}
+}
---
+++ Multi.java
@@ -0,0 +1,81 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+public class MultipleMovingWindow extends JFrame implements Runnable {
+	private static Point psion = new Point();
+	double velx,vely;
+	int delay=50;
+	Dimension d;
+	int wid,ht;
+	public MultipleMovingWindow(double velx,double vely,Dimension d) {
+		super("Dancing Window");
+		this.velx=velx;
+		this.vely=vely;
+		this.d=d;
+		wid=d.width;
+		ht=d.height;
+
+		setSize(100,100);
+	}
+
+	public void run()
+	{
+		while(true)	
+		{
+			Point p=getLocationOnScreen();
+			input(p);
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e1) {
+				e1.printStackTrace();
+			}
+			p.setLocation(p.x+velx,p.y+vely);
+			
+			//Check if touched the wall
+			if(p.x<0 || p.x>wid-100)
+			{
+				System.out.println("Hit side wall "+p.x+ " "+wid);
+				p.setLocation(p.x-2*velx,p.y);
+				velx*=-1;
+			}
+
+			if(p.y<20 || p.y>ht-100)
+			{
+				System.out.println("Hit horizontal wall "+p.y+ " "+ht);
+				p.setLocation(p.x,p.y-2*vely);
+				vely*=-1;
+			}
+			
+			//Check if bumped each other
+			if(isBumped(p)){
+				System.out.println("Bumped!");
+			}
+
+			this.setLocation(p);
+		}
+
+
+	}
+	private synchronized void input(Point p){
+		psion = p;
+	}
+	private synchronized boolean isBumped (Point p) {
+		
+		if((psion.x < p.x && p.x < psion.x + 100 )&& (psion.y < p.y && p.y < psion.y + 100)){
+			return true;
+		}
+		return false;
+	}
+	public void runAnimation() {
+		(new Thread(this)).start();
+	}
+
+	public static Point getPsion() {
+		return psion;
+	}
+
+	public static void setPsion(Point psion) {
+		MultipleMovingWindow.psion = psion;
+	}
+}
